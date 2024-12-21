d1059a0367bc399742d038373b1f35b93ce75802
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-21 10:49:24 -0500

    Toggle Context

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Star from "./Star"
-import Toggle from './Toggle';
+import Toggle from './Toggle/Toggle';
 
 function App() {
 

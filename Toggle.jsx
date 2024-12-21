8a0099c2a4f8b6424d9ba7cbd2e04a3e195d8f58
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-21 10:32:15 -0500

    setup

--- Toggle.jsx
+++ Toggle.jsx
@@ -1,9 +1,19 @@
-/**
- * Challenge:
- * 
- * 1. Create a Toggle component that initializes its own boolean state.
- * 2. Create a function called `toggle` in that component that flips the 
- *    boolean when the function runs.
- * NOTE: Don't worry about rendering anything yet
- */
+import React from "react"
+
+export default function Toggle({ children }) {
+
+    const [toggled, setToggled] = React.useState(false)
+
+    function toggle() {
+        setToggled((prevState => {
+            return !prevState
+        }))
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
 

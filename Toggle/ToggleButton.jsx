1aebf5619f9f0daa863120ed0281cfbe3277911c
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-21 13:59:37 -0500

    File Restructured for components

--- Toggle/ToggleButton.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Children } from "react";
-import { ToggleContext } from "./Toggle";
-
-export default function ToggleButton({ children }) {
-
-    const context = React.useContext(ToggleContext);
-
-    return <div onClick={context.toggle}>{children}</div>;
-}
--- index.jsx
+++ index.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Star from "./Star"
-import Toggle from './Toggle/index';
+import Toggle from "./components/Toggle/index"
 import { BsStar, BsStarFill } from "react-icons/bs"
 
-/**
- * Challenge: refactor our app to use the Toggle.On 
- * and Toggle.Off components to conditionally render the
- * star icons from react-icons. (See Star.js for that code).
- * 
- * Reminders:
- * 1. Make sure to copy over the classNames from Star.js too!
- *    I've added some CSS that we don't want to lose.
- * 2. Clicking the Star should flip the Toggle state. Try to
- *    figure out how you can do that 🤔. Hint: we built something
- *    that lets us flip the Toggle state back and forth 😉
- */
-
 function App() {
+    /**
+     * Challenge: Refactor the Menu components to use the logic 
+     * from Toggle instead of doing any logic of its own.
+     * 
+     * 1. Remove all context and state logic from the menu components
+     * 2. Using Toggle and its "sub-components", add a menu to this
+     *    App component below the Star. Make sure to use a separate
+     *    <Toggle> wrapper so the state isn't tied to the Star's 
+     *    Toggle state
+     */
 
     return (
         <>
             <Toggle>
                 <Toggle.Button>
-                    <Toggle.On> <BsStarFill className="star filled" /> </Toggle.On>
-                    <Toggle.Off> <BsStar className="star " /> </Toggle.Off>
+                    <Toggle.On>
+                        <BsStarFill className="star filled" />
+                    </Toggle.On>
+                    <Toggle.Off>
+                        <BsStar className="star" />
+                    </Toggle.Off>
                 </Toggle.Button>
             </Toggle>
+
+            {/* Your Menu code goes here */}
+
         </>
     )
 }

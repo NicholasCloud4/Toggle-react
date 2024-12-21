5959cc16801b2f9648f527c81316db10ba81652e
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-21 14:30:45 -0500

    Toggle Menu Component

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Toggle from "./components/Toggle/index"
+import Menu from "./components/Menu/index"
 import { BsStar, BsStarFill } from "react-icons/bs"
 
 function App() {
@@ -19,16 +20,27 @@ function App() {
         <>
             <Toggle>
                 <Toggle.Button>
-                    <Toggle.On>
-                        <BsStarFill className="star filled" />
-                    </Toggle.On>
-                    <Toggle.Off>
-                        <BsStar className="star" />
-                    </Toggle.Off>
+                    <Toggle.On> <BsStarFill className="star filled" /> </Toggle.On>
+                    <Toggle.Off> <BsStar className="star" /> </Toggle.Off>
                 </Toggle.Button>
             </Toggle>
 
             {/* Your Menu code goes here */}
+            <Toggle>
+                <Menu>
+                    <Toggle.Button>
+                        <Menu.Button>Menu</Menu.Button>
+                    </Toggle.Button>
+                    <Toggle.On>
+                        <Menu.Dropdown>
+                            <Menu.Item>Home</Menu.Item>
+                            <Menu.Item>About</Menu.Item>
+                            <Menu.Item>Contact</Menu.Item>
+                            <Menu.Item>Blog</Menu.Item>
+                        </Menu.Dropdown>
+                    </Toggle.On>
+                </Menu>
+            </Toggle>
 
         </>
     )

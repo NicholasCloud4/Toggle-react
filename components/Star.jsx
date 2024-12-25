5997fdb9dda2841b2a397141e1c3575c07849859
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-25 15:50:57 -0500

    Part 3

--- components/Star.jsx
+++ components/Star.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Toggle from './Toggle/index'
 import { BsStar, BsStarFill } from 'react-icons/bs'
+import useToggle from '../hooks/useToggle'
 
 export default function Star({ onChange }) {
     /**
-     * Challenge:
-     * 1. Receive a prop called onChange and pass it to the
-     *    Toggle's onToggle function
-     * 2. Add an onChange prop to the Star component instance
-     *    in index.js that just runs a console.log() for now.
-     */
+ * Challenge: Remove all parts of our Toggle component
+ * (don't delete the react-icons stars though!) and use 
+ * the useToggle hook to conditionally render the stars. 
+ * Make sure to add an onClick that runs the `toggle` function
+ * you get from useToggle()
+ */
+
+    const [on, toggle] = useToggle(false)
+
+
     return (
-        <Toggle onToggle={onChange}>
-            <Toggle.Button>
-                <Toggle.On> <BsStarFill className="star filled" /> </Toggle.On>
-                <Toggle.Off> <BsStar className="star" /> </Toggle.Off>
-            </Toggle.Button>
-        </Toggle>
+        <div onClick={toggle}>
+            {on === true ? <BsStarFill className="star filled" /> : <BsStar className="star" />}
+        </div>
     )
 }
